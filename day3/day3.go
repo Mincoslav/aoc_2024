25ca98e518d783ff6f6ed8f6dea7a011abc27aa7
Skap <43174609+Mincoslav@users.noreply.github.com>
2025-03-09 23:16:35 +0100

    day3 solution. regex learning

--- day3/day3.go
+++ day3/day3.go
@@ -1,9 +1,45 @@
 package main
 
-import "../utils"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"github.com/mincoslav/aoc/utils"
+)
 
-func main()  {
-	// getScannerForFile()
-	utils.getScannerForFile()
-	// aoc.getScannerForFile("./input.txt")
+func findRegexMatches(input string, regexExpression string) []string {
+	regex := regexp.MustCompile(regexExpression)
+	return regex.FindAllString(input, -1)
+}
+
+func main() {
+	var file = utils.GetFileContentAsString("./input.txt")
+	var multiplicationSums int64 = 0
+
+	// 	// Part 2:
+	var invalidMultiplications = findRegexMatches(file, `don't\(\)(.*?)((do\(\))|(\n.*?do\(\)))`)
+	// fmt.Printf("len(invalidMultiplications): %v\n", len(invalidMultiplications))
+
+	for _, invalid_string := range invalidMultiplications {
+		file = strings.ReplaceAll(file, invalid_string, "")
+	}
+	// End of Part 2
+
+	// main part -------------------
+	var results = findRegexMatches(file, `mul\(\d{1,3},\d{1,3}\)`)
+		fmt.Printf("len(results): %v\n", len(results))
+		for _, result := range results {
+
+			var numbers = findRegexMatches(result, `\d{1,3}`)
+			left_int, left_error := strconv.ParseInt(numbers[0], 10, 64)
+			right_int, right_error := strconv.ParseInt(numbers[1], 10, 64)
+
+			if left_error == nil && right_error == nil {
+				multiplicationSums += (left_int * right_int)
+			}
+
+		}
+
+	fmt.Printf("multiplicationSums: %v", multiplicationSums)
 }
